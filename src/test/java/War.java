2fcea7795bfa4c7b4e76d1645bc3c2a23148c596
qwer083 <Qwertyiop890>
2022-10-30 20:05:39 +0300

    finita

--- src/test/java/War.java
+++ src/test/java/War.java
@@ -3,6 +3,8 @@ public class War {
         int a = 1684000;
         int b = 85150000;
         int c = a+b;
+        System.out.println("количество пехотинцев у Ланнистеров: " + a);
+        System.out.println("количество орков в войске Сарумана: " + b);
         System.out.println("общее количество пехоты: " + c);
     }
 }
