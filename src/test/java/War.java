8004fba72ad0d5c2db2c82df0b872699f4fc7bba
qwer083 <Qwertyiop890>
2022-10-30 20:04:16 +0300

    finita

--- src/test/java/War.java
+++ src/test/java/War.java
@@ -3,7 +3,6 @@ public class War {
         int a = 1684000;
         int b = 85150000;
         int c = a+b;
-        String s = "общее количество пехоты";
         System.out.println("общее количество пехоты: " + c);
     }
 }
