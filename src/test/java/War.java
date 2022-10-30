c73e379c79ced4d2904402c426b3207a57f239d4
qwer083 <Qwertyiop890>
2022-10-30 17:50:23 +0300

    Initial commit

---
+++ src/test/java/War.java
@@ -0,0 +1,11 @@
+public class War {
+    public static void main(String []args) {
+        int a = 1684000;
+        int b = 85150000;
+        int c = a+b;
+        String s = "общее количество пехоты";
+        System.out.println("общее количество пехоты: " + c);
+    }
+}
+
+
